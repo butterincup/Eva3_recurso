d5d8e259487dca2ff9fab01e9b63ab2d10c35850
butterincup <143638043+butterincup@users.noreply.github.com>
2024-04-29 15:51:35 -0700

    Add files via upload

---
+++ Eva3_7_cubo/src/eva3_7_cubo/Eva3_7_cubo.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_7_cubo;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Eva3_7_cubo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int cubo [][][] = new int [10][5][3];
+        for (int i = 0; i < cubo.length; i++) {
+            for (int j = 0; j < cubo[i].length; j++) {
+                for (int k = 0; k < cubo[i][j].length; k++) {
+                cubo [i][j][k] = (int)(Math.random()*100);
+                    System.out.println("matriz["+i+"]["+j+"]["+k+"] = "+ cubo[i][j][k]);
+                }
+            }
+        }
+       
+
+        
+        
+    }
+    
+}
